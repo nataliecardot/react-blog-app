8d237f4645ddeb9edaae2d73dc739e4f783276d7
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-22 13:55:28 -0700

    Add home component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,14 @@
 import './App.css';
+import Home from './Home';
 import Navbar from './Navbar';
 
 function App() {
-  const title = 'Welcome to the new blog';
-  const likes = 50;
-
   return (
     // JSX template
     <div className="App">
       <Navbar />
       <div className="content">
-        <h1>{title}</h1>
-        <p>Liked {likes} times</p>
+        <Home />
       </div>
     </div>
   );
