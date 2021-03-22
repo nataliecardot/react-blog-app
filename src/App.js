b4bb481dd6bb546fff97e0af7aca40c147c8098b
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-22 13:53:25 -0700

    Add navbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import Navbar from './Navbar';
 
 function App() {
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+
   return (
+    // JSX template
     <div className="App">
+      <Navbar />
       <div className="content">
-        <h1>App Component</h1>
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
       </div>
     </div>
   );
