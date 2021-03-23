487f786ef51ef103a573f368b724b5e683523c15
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-23 11:52:12 -0700

    Enable post deletion

--- src/Home.js
+++ src/Home.js
@@ -18,12 +18,18 @@ const Home = () => {
     },
   ]);
 
+  const handleDelete = (id) => {
+    const newBlogs = blogs.filter((blog) => blog.id !== id);
+    setBlogs(newBlogs);
+  };
+
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All Posts" />
       <BlogList
         blogs={blogs.filter((blog) => blog.author === 'Jane')}
         title="Jane's Posts"
+        handleDelete={handleDelete}
       />
     </div>
   );
