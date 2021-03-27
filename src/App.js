477a522def6fad895adc914d91dfddcd09656160
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-26 21:41:08 -0700

    Use router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import Home from './Home';
 import Navbar from './Navbar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     // JSX template
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          {/* Switch component ensures that only one route shows at any one time */}
+          <Switch>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
