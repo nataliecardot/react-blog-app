d05a2d15388c8fc91b0f36c09a847b45b0848ea2
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-27 07:48:24 -0700

    Use router links

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,13 @@
+import { Link } from 'react-router-dom';
+
 const Navbar = () => {
   return (
     <nav className="navbar">
       <h1>React Blog</h1>
       <div className="links">
-        <a href="/">Home</a>
-        <a href="create">New Post</a>
+        {/* Links tags allow router to intercept request to server, instead matching path against routes */}
+        <Link to="/">Home</Link>
+        <Link to="/create">New Post</Link>
       </div>
     </nav>
   );
