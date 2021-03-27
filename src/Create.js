75bfa695720bcf45274814fc4dfd21f5d520b3c2
Natalie-Cardot <nataliecardot@gmail.com>
2021-03-27 11:31:39 -0700

    Use controlled inputs for form

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,40 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('Jane');
+
   return (
     <div className="create">
       <h2>Add a new post</h2>
+      <form>
+        <label htmlFor="blogTitle">Blog title:</label>
+        <input
+          id="blogTitle"
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label htmlFor="blogBody">Blog body:</label>
+        <textarea
+          id="blogBody"
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <label htmlFor="blogAuthor"></label>
+        <select
+          id="blogAuthor"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+        >
+          <option value="Jane">Jane</option>
+          <option value="William">William</option>
+        </select>
+        <button>Add Post</button>
+      </form>
     </div>
   );
 };
